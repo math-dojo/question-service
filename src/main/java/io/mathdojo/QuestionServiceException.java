cb4af5b9250d5e6d9258832b812e3083fa244812
Ade <aakano684>
2020-12-30 22:56:41 +0000

    trigger build

--- src/main/java/io/mathdojo/QuestionServiceException.java
+++ src/main/java/io/mathdojo/QuestionServiceException.java
@@ -2,9 +2,6 @@ package io.mathdojo;
 
 public class QuestionServiceException extends RuntimeException {
 
-	/**
-	 * 
-	 */
 	public static String NOT_FOUND_MESSAGE = "Object with supplied id was not found";
 	public static String UNSUPPORTED_MESSAGE = "Unsupported query";
 	public static String ALREADY_EXISTS_MESSAGE = "Object with supplied id already exists";
